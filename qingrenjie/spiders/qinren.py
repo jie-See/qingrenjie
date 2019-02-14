f3d02edd7964578914020f4871cdaba1303ba778
shizhijie <739698292@qq.com>
2019-02-14 14:22:45 +0800

    fitst commit

---
+++ qingrenjie/spiders/qinren.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import json
+
+import scrapy
+from scrapy.http import FormRequest
+from qingrenjie.items import QingrenjieItem
+
+
+class QinrenSpider(scrapy.Spider):
+    name = 'qinren'
+    allowed_domains = ['xcx1.sangcr.com']
+    start_urls = ['http://xcx1.sangcr.com/']
+    headers = {
+        ""
+    }
+    formdata = {
+        'category_id': '758',
+        'topic_limit': '1000',
+        'session_key': 'zxdt_n9uqrktf4cepdkeezwnzhuayh6qhmp9w'
+    }
+
+    def start_requests(self):
+        url = "https://xcx1.sangcr.com/api/app/getQuestion"
+        requests = []
+        request = FormRequest(url, callback=self.parse, formdata=self.formdata)
+        requests.append(request)
+        print(requests)
+
+        return requests
+
+    def parse(self, response):
+
+        response = json.loads(response.text)["topics"]
+        for a in response:
+            item = QingrenjieItem()
+            item["title"] = a['title']
+            answer = []
+            b = a['list']
+            num = len(b)
+            if 0 < num:
+                item['A'] = b[0]['title']
+            if 1 < num:
+                item['B'] = b[1]['title']
+            if 2 < num:
+                item['C'] = b[2]['title']
+            if 3 < num:
+                item['D'] = b[3]['title']
+                # answer.append(b['title'])
+            # item["answer"] = answer
+            yield item
